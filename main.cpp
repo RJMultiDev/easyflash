27a22bce0001563765c59991479f82d5bde48b79
MultiDev <2815968613@qq.com>
2025-10-18 21:30:12 +0800

    new

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    //load arg
+    string fn = "main.ef";
+    if (argc == 2) fn = *argv[1];
+    int err = do_script(fn);
+    return err;
+}
+
+int do_script(string file)
+{
+    return 0;
+}
